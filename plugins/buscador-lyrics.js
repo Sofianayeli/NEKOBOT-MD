0d172667eb7daae166a67a8653bc1f7b53f90d47
Sofianayeli <107886031+Sofianayeli@users.noreply.github.com>
2022-06-20 19:07:46 -0300

    Add files via upload

---
+++ plugins/buscador-lyrics.js
@@ -0,0 +1,39 @@
+import fetch from 'node-fetch'
+import { lyrics, lyricsv2 } from '@bochilteam/scraper'
+import { googleImage } from '@bochilteam/scraper'
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : ''
+if (!teks) throw `${mg}𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙀𝙇 𝙉𝙊𝙈𝘽𝙍𝙀 𝘿𝙀 𝙐𝙉𝘼 𝘾𝘼𝙉𝘾𝙄𝙊𝙉 𝙋𝘼𝙍𝘼 𝙊𝘽𝙏𝙀𝙉𝙀𝙍 𝙇𝘼 𝙇𝙀𝙏𝙍𝘼\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command} Runaway*\n\n𝙀𝙉𝙏𝙀𝙍 𝙏𝙃𝙀 𝙉𝘼𝙈𝙀 𝙊𝙁 𝘼 𝙎𝙊𝙉𝙂 𝙏𝙊 𝙂𝙀𝙏 𝙏𝙃𝙀 𝙇𝙔𝙍𝙄𝘾𝙎\n𝙀𝙓𝘼𝙈𝙋𝙇𝙀\n*${usedPrefix + command} Billie Eilish bored*`
+const result = await lyricsv2(teks).catch(async _ => await lyrics(teks))
+let res = await fetch(global.API('https://some-random-api.ml', '/lyrics', {
+title: result.author + result.title}))
+if (!res.ok) throw await res.text()
+let json = await res.json()
+if (!json.thumbnail.genius) throw json
+
+
+let letratexto =`
+𝙏𝙄𝙏𝙐𝙇𝙊 | 𝙏𝙄𝙏𝙇𝙀 
+🔥 *${result.title}*
+
+𝘼𝙐𝙏𝙊𝙍(𝘼) | 𝘼𝙐𝙏𝙃𝙊𝙍
+🔥 *${result.author}*
+
+
+${result.lyrics}
+
+
+𝙀𝙉𝙇𝘼𝘾𝙀 | 𝙐𝙍𝙇
+💖 *${result.link}*
+`.trim()
+conn.sendHydrated(m.chat, letratexto, wm, json.thumbnail.genius, null, null, null, null, [
+['𝘿𝙚𝙨𝙘𝙖𝙧𝙜𝙖𝙧 | 𝘿𝙤𝙬𝙣𝙡𝙤𝙖𝙙 🚀', `/play ${text}`],
+['𝙈𝙚𝙣𝙪 𝘽𝙪𝙨𝙦𝙪𝙚𝙙𝙖𝙨 | 𝙎𝙚𝙖𝙧𝙘𝙝𝙚𝙨 🔎', '#buscarmenu'],
+['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu']
+], m)
+}
+handler.help = ['lirik','letra'].map(v => v + ' <Apa>')
+handler.tags = ['internet']
+handler.command = /^(lirik|lyrics|lyric|letra)$/i
+handler.exp = 100
+export default handler 
---
+++ plugins/descargas-pinterest.js
@@ -0,0 +1,18 @@
+import { pinterest } from '@bochilteam/scraper'
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+if (!text) throw `${mg} 𝙐𝙎𝙀 𝘿𝙀 𝙇𝘼 𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 𝙈𝘼𝙉𝙀𝙍𝘼\n*${usedPrefix + command} neko*\n\n𝙐𝙎𝙀 𝙏𝙃𝙀 𝘾𝙊𝙈𝙈𝘼𝙉𝘿 𝙇𝙄𝙆𝙀 𝙏𝙃𝙄𝙎\n*${usedPrefix + command} Cat*` 
+const json = await pinterest(text)
+await conn.sendFile(m.chat, json.getRandom(), 'error.jpg', `
+╰⊱✅⊱ *𝙀́𝙓𝙄𝙏𝙊 | 𝙎𝙐𝘾𝘾𝙀𝙎𝙎* ⊱✅⊱╮`.trim(), m)
+  
+  conn.sendHydrated(m.chat, `💞 𝙍𝙚𝙨𝙪𝙡𝙩𝙖𝙙𝙤 | 𝙍𝙚𝙨𝙪𝙡𝙩: ${text}`, `𝙋𝙞𝙣𝙩𝙚𝙧𝙚𝙨𝙩 | ${wm}`, null, md, '𝗡𝗘𝗞𝗢𝗕𝗢𝗧-𝗠𝗗', null, null, [
+['🔄 𝙎𝙞𝙜𝙪𝙞𝙚𝙣𝙩𝙚 | 𝙉𝙚𝙭𝙩', `/pinterest ${text}`],
+['🔍 𝙂𝙤𝙤𝙜𝙡𝙚 ', `#image ${text}`],
+['✅ 𝙈𝙚𝙣𝙪', `.menu`],  
+], m)
+                    }
+handler.help = ['pinterest <keyword>']
+handler.tags = ['internet']
+handler.command = /^(pinterest|dlpinterest|pinterestdl)$/i
+handler.exp = 100
+export default handler
