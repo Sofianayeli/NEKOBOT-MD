81f9d58949a366fbc817a7aa28f0a8637b852822
Sofianayeli <107886031+Sofianayeli@users.noreply.github.com>
2022-06-20 19:10:31 -0300

    Add files via upload

---
+++ plugins/info-cuentas-oficiales.js
@@ -0,0 +1,22 @@
+let media = './media/menus/Menuvid2.mp4'
+let handler = async (m, { conn, command }) => {
+let str = `
+💙 𝘽𝙄𝙀𝙉𝙑𝙀𝙉𝙄𝘿𝙊(𝘼) 𝘼 𝙇𝘼𝙎 𝘾𝙐𝙀𝙉𝙏𝘼𝙎 𝙊𝙁𝙄𝘾𝙄𝘼𝙇𝙀𝙎
+💜 𝙒𝙀𝙇𝘾𝙊𝙈𝙀 𝙏𝙊 𝙏𝙃𝙀 𝙊𝙁𝙁𝙄𝘾𝙄𝘼𝙇 𝘼𝘾𝘾𝙊𝙐𝙉𝙏𝙎
+┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+✅ *INSTAGRAM*
+*${md}*
+┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+*Si tienen dudas, sugerencias, o preguntas solo escriban por Instagram.*\n
+*If you have doubts, suggestions or questions just write on Instagram.*
+`.trim()
+  
+conn.sendHydrated(m.chat, str, wm, media, 'https://github.com/Sofiaperez18/NEKOBOT-MD.git', '𝗡𝗘𝗞𝗢𝗕𝗢𝗧-𝗠𝗗', null, null, [
+['𝙂𝙧𝙪𝙥𝙤𝙨 𝙊𝙛𝙞𝙘𝙞𝙖𝙡𝙚𝙨 | 𝙂𝙧𝙤𝙪𝙥𝙨 🔰', '.grupos'],
+['𝘾𝙧𝙚𝙖𝙙𝙤𝙧𝙖 | 𝘾𝙧𝙚𝙖𝙩𝙤𝙧 🥰', '#owner'],
+['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu']
+], m,)}
+
+handler.command = /^cuentasoficiales|nekoig|cuentanb|cuentanb|accounts|nekooccounts|account|igneko|cuentasdeneko|cuentasdenekobot|cuentanekobot|cuentasnekobot$/i
+handler.exp = 100
+export default handler
---
+++ plugins/info-infobot.js
@@ -0,0 +1,80 @@
+import os from 'os'
+import util from 'util'
+import sizeFormatter from 'human-readable'
+import MessageType from '@adiwajshing/baileys'
+import fs from 'fs'
+import { performance } from 'perf_hooks'
+let handler = async (m, { conn, usedPrefix }) => {
+let _uptime = process.uptime() * 1000
+let uptime = clockString(_uptime) 
+let totalreg = Object.keys(global.db.data.users).length
+const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
+const groupsIn = chats.filter(([id]) => id.endsWith('@g.us'))
+const groups = chats.filter(([id]) => id.endsWith('@g.us'))
+const used = process.memoryUsage()
+const { restrict } = global.db.data.settings[conn.user.jid] || {}
+const { autoread } = global.opts
+let pp = './media/menus/Menu1.jpg'
+let old = performance.now()
+let neww = performance.now()
+let speed = neww - old
+
+let info = `
+╭━━━━[ ${gt} ]━━━━━⬣
+┃
+┃➥ *CREADORA | CREATOR*
+┃ꨄ︎ *𝗡𝗘𝗞𝗢𝗕𝗢𝗧-𝗠𝗗*
+┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃➥ *CONTACTO | CONTACT* 
+┃ꨄ︎ *${ig}*
+┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃🔮 *VERSIÓN ACTUAL | VERSION*
+┃ꨄ︎ ${vs}
+┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃➥ *PREFIJO | PREFIX*
+┃ꨄ︎ *${usedPrefix}*
+┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃➥ *CHATS PRIVADOS | PRIVATE CHAT*
+┃ꨄ︎ *${chats.length - groups.length}*
+┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃➥ *CHATS DE GRUPOS | GROUP CHAT*
+┃ꨄ︎ *${groups.length}* 
+┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃➥ *CHATS EN TOTAL | TOTAL CHATS*
+┃ꨄ︎ *${chats.length}* 
+┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃➥ *ACTIVIDAD | ACTIVITY*
+┃ꨄ︎ *${uptime}*
+┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃➥ *USUARIOS | USERS*
+┃ꨄ *${totalreg}* 
+┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃➥*VELOCIDAD | SPEED*
+┃ꨄ︎  *${speed}*
+┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃➥ *AUTOREAD*
+┃ꨄ︎ ${autoread ? '*Activado ✔*' : '*Desactivado ✘*'}
+┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃➥ *RESTRICT* 
+┃ꨄ︎ ${restrict ? '*Activado ✔*' : '*Desactivado ✘*'} 
+┃
+╰━━━[ 𝙄𝙣𝙛𝙤𝙧𝙢𝙖𝙘𝙞ó𝙣 | 𝙄𝙣𝙛𝙤𝙧𝙢𝙖𝙩𝙞𝙤𝙣 ]━━⬣`.trim()
+
+conn.sendHydrated(m.chat, info, wm, pp, 'https://github.com/Sofiaperez18/NEKOBOT-MD.git', '𝗡𝗘𝗞𝗢𝗕𝗢𝗧-𝗠𝗗', null, null, [
+['𝙑𝙚𝙧 𝙂𝙧𝙪𝙥𝙤𝙨 | 𝙎𝙚𝙚 𝙂𝙧𝙤𝙪𝙥𝙨', '#grupolista'],
+['𝘾𝙪𝙚𝙣𝙩𝙖𝙨 𝙊𝙛𝙞𝙘𝙞𝙖𝙡𝙚𝙨 | 𝘼𝙘𝙘𝙤𝙪𝙣𝙩𝙨', '/cuentasgb'],
+['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪', '.menu']
+], m,)
+//conn.reply(m.chat, info, m)
+}
+handler.help = ['infobot']
+handler.tags = ['info', 'tools']
+handler.command = /^(infobot|informacionbot|infoneko|informaciónneko|informacionneko)$/i
+export default handler
+
+function clockString(ms) {
+let h = Math.floor(ms / 3600000)
+let m = Math.floor(ms / 60000) % 60
+let s = Math.floor(ms / 1000) % 60
+console.log({ms,h,m,s})
+return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')}
